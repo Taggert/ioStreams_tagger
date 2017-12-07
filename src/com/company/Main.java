d413a0df390610971a9fa5236a1eb19e070ac153
Taggert <4uxpux@mail.ru>
2017-12-07 19:38:54 +0200

    Added input from keyboard and saving to file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,36 @@ package com.company;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         //InputStream inputStream = new URL("https://google.com").openStream();
-        File file = new File("D:\\Humster.txt");
-        InputStream inputStream = new FileInputStream(file);
-       /*Scanner scanner = new Scanner(System.in);
-       String str = scanner.nextLine();
-        InputStream inputStream = str;*/
+        /*File file = new File("D:\\Humster.txt");
+        InputStream inputStream = new FileInputStream(file);*/
+        InputStreamReader reader = new InputStreamReader(System.in);
+        OutputStream output = new FileOutputStream("D:\\test.txt");
+        /*Scanner scanner = new Scanner(System.in);*/
+        System.out.println("Input text, if you type enter, text will be saved in the file");
 
-        OutputStream outputStream = System.out;
+        char symb;
+        do{
+            symb = (char) reader.read();
+            output.write(symb);
+        }while(symb!='\n');
 
-        copy(inputStream, outputStream);
-       // write(inputStream, outputStream);
+
+
+
+        reader.close();
+        output.close();
+
+
+        //copy(inputStream, outputStream);
+        // write(inputStream, outputStream);
 
 
     }
@@ -39,7 +53,7 @@ public class Main {
 
     private static void copy(InputStream inputStream, OutputStream outputStream) throws IOException {
         byte[] buff = new byte[64 * 1024];
-        while(inputStream.available()>0){
+        while (inputStream.available() > 0) {
             int count = inputStream.read(buff);
             outputStream.write(buff, 0, count);
         }
